322bfd60ab8b6db4ae274c24c5cccb055d0602e0
Pavel Artemev <paveldotstream@gmail.com>
2024-07-05 22:16:37 +0300

    _add database for tasks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # import conf
-from authenticate import start
+from add_tasks import foreign_key, add_task
+from authenticate import register_user, authenticate_user
+from print_tasks import print_task
+
 
 class Task:
     def __init__(self, task):
@@ -8,7 +11,8 @@ class Task:
 
 class TODOList:
     def __init__(self):
-        self.tasks = dict()  #In future i will have to replace the sheet with the database
+        # self.tasks = dict()  # In future i will have to replace the sheet with the database
+        self.name = ''
 
     def new_task(self, task):
         self.task = task
@@ -20,39 +24,56 @@ class TODOList:
         self.status = status
 
     def add_task(self):
-        self.new_task(input('Введите новую задачу: '))
-        self.description_task(input('Введите описание задачи: '))
-        self.mark()
-        self.tasks[self.task] = {'id' : len(self.tasks), 'описание': self.description, 'статус': self.status}
+        task = input('Напишите новую задачу:\n ')
+        describtion = input('Напишите описание к задаче:\n ')
+        name = self.name
+        add_task(task=task, description=describtion, name_user=name)
+        # self.new_task(input('Введите новую задачу: '))
+        # self.description_task(input('Введите описание задачи: '))
+        # self.mark()
+        # self.tasks[self.task] = {'id': len(self.tasks), 'описание': self.description, 'статус': self.status}
 
+    # def print_tasks(self):
+    #     print(self.tasks)
 
     def run(self):
-        start()
-        while True:
-            print("\nTODO List:")
-            if self.tasks == False:
-                print('List is empty(Список пустой)')
-            else:
-                self.print_tasks()
+        choice = 0
+        while choice != 1:
+            choice = int(input('Выберете вариант:\n 1.Войти \n2.Зарегистрироваться:\n3. Выход:\n  '))
+            if choice == 2:
+                register_user(input('Введите свой email'), input('Введите свое имя '), input('Введите свой пароль '))
+            elif choice == 1:
+                self.name = input("Введите имя пользователя: ")
+                password = input("Введите пароль: ")
+                authenticate_user(self.name, password)
+                print("Аутентификация успешна!")
+                while True:
+                    print("\nTODO List:")
+                    # if self.tasks == False:
+                    #     print('List is empty(Список пустой)')
+                    # else:
+                    #     self.print_tasks()
 
-            print("\nMenu:")
-            print("1. Добавить задачу")
-            print("2. Изменить статус задачи")
-            print("3. Выход")
+                    print("\nMenu:")
+                    print("1. Добавить задачу")
+                    # print("2. Изменить статус задачи")
+                    print("3. Показать мои задачи. ")
+                    print("4. Выход")
 
-            choice = input("Enter your choice : ")
+                    choice = input("Enter your choice : ")
 
-            if choice == '1':
-                self.add_task()
-            elif choice == '2':
-                new_mark = int(input('Введите номер задачи: '))
-                self.tasks[list(self.tasks.keys())[new_mark]]['статус'] = True
-
-            elif choice == '3':
-                print("Exiting...")
-                break
-            else:
-                print("Неверный выбор. Пожалуйста введите 1, 2, или 3.")
+                    if choice == '1':
+                        self.add_task()
+                    elif choice == '2':
+                        new_mark = int(input('Введите номер задачи: '))
+                        self.tasks[list(self.tasks.keys())[new_mark]]['статус'] = True
+                    elif choice == '3':
+                        print(*print_task(foreign_key(self.name)), sep= '\n')
+                    elif choice == '4':
+                        print("Exiting...")
+                        break
+                    else:
+                        print("Неверный выбор. Введите верный номер.")
 
 
 def main():
